6b921b0e954e01096dc999eb3ce138f32322e49b
Pieter Bos <pieter.bos@nedap.com>
2016-10-12 22:04:09 +0200

    Remove absolute paths from generate_trombone script

--- generate_trombone.sh
+++ generate_trombone.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 mkdir -p output
-/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o output/bell_top.stl -Dpart=\"bell_top\" ~/Documents/OpenSCAD/bell/r4f/bell.scad &
-/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o output/bell_middle_1.stl -Dpart=\"bell_middle\" ~/Documents/OpenSCAD/bell/r4f/bell.scad &
-/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o output/bell_middle_2.stl -Dpart=\"bell_middle_2\" ~/Documents/OpenSCAD/bell/r4f/bell.scad &
-/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o output/bell_bottom_1.stl -Dpart=\"bell_bottom_1\" ~/Documents/OpenSCAD/bell/r4f/bell.scad &
-/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o output/bell_bottom_2.stl -Dpart=\"bell_bottom_2\" ~/Documents/OpenSCAD/bell/r4f/bell.scad &
-/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o output/neckpipe_bottom.stl -Dpart=\"neckpipe_bottom\" ~/Documents/OpenSCAD/bell/r4f/bell.scad &
-/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o output/neckpipe_top.stl -Dpart=\"neckpipe_top\" ~/Documents/OpenSCAD/bell/r4f/bell.scad &
-# /Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o output/tuning_slide.stl -Dpart=\"tuning_slide\" ~/Documents/OpenSCAD/bell/r4f/bell.scad &
-/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o output/connection_bottom.stl -Dpart=\"connection_bottom\" ~/Documents/OpenSCAD/bell/r4f/bell.scad & 
-/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o output/connection_top.stl -Dpart=\"connection_top\" ~/Documents/OpenSCAD/bell/r4f/bell.scad &
+OpenSCAD -o output/bell_top.stl -Dpart=\"bell_top\" ./bell.scad &
+OpenSCAD -o output/bell_middle_1.stl -Dpart=\"bell_middle\" ./bell.scad &
+OpenSCAD -o output/bell_middle_2.stl -Dpart=\"bell_middle_2\" ./bell.scad &
+OpenSCAD -o output/bell_bottom_1.stl -Dpart=\"bell_bottom_1\" ./bell.scad &
+OpenSCAD -o output/bell_bottom_2.stl -Dpart=\"bell_bottom_2\" ./bell.scad &
+OpenSCAD -o output/neckpipe_bottom.stl -Dpart=\"neckpipe_bottom\" ./bell.scad &
+OpenSCAD -o output/neckpipe_top.stl -Dpart=\"neckpipe_top\" ./bell.scad &
+OpenSCAD -o output/tuning_slide.stl -Dpart=\"tuning_slide\" ./bell.scad &
+OpenSCAD -o output/connection_bottom.stl -Dpart=\"connection_bottom\" ./bell.scad & 
+OpenSCAD -o output/connection_top.stl -Dpart=\"connection_top\" ./bell.scad &
