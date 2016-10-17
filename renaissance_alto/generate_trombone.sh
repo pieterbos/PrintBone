360aad573abc0f03b784262be2424c8f275895d0
Pieter Bos <pieter.bos@nedap.com>
2016-10-18 00:37:32 +0200

    Fix script, add readme

--- renaissance_alto/generate_trombone.sh
+++ renaissance_alto/generate_trombone.sh
@@ -3,5 +3,5 @@ mkdir -p output
 OpenSCAD -o output/bell_top.stl -Dpart=\"bell_top\" ./bell.scad &
 OpenSCAD -o output/bell_middle.stl -Dpart=\"bell_middle\" ./bell.scad &
 OpenSCAD -o output/bell_bottom.stl -Dpart=\"bell_bottom\" ./bell.scad &
-#OpenSCAD -o output/neckpipe.stl -Dpart=\"neckpipe\" ./bell.scad &
-#OpenSCAD -o output/tuning_slide.stl -Dpart=\"tuning_slide\" ./bell.scad &
+OpenSCAD -o output/neckpipe.stl -Dpart=\"neckpipe\" ./bell.scad &
+OpenSCAD -o output/tuning_slide.stl -Dpart=\"tuning_slide\" ./bell.scad &
